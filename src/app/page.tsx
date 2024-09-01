d2e8c35cb0dbe9918e8516946771c1587a1d1006
Alan Anaya <hello@alananaya.dev>
2024-08-31 20:14:41 -0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,139 @@
+"use client";
+import DollarIcon from "@/images/icon-dollar.svg";
+import PeopleIcon from "@/images/icon-person.svg";
 import Image from "next/image";
-
+import { useState } from "react";
 export default function Home() {
+  const [tipAmount, setTipAmount] = useState(0);
+  const [personAmount, setPersonAmount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [numberPeople, setNumberPeople] = useState(0);
+  const [tipPercentage, setTipPercentage] = useState(0);
+
+  const handleTotalAmount = (event: React.FormEvent<HTMLInputElement>) => {
+    setTotalAmount(parseFloat(event.currentTarget.value));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center md:px-48 sm:px-0 bg-light-grayish-cyan">
+      <h2 className="text-very-dark-cyan my-20 tracking-wider text-2xl ">
+        S P L I
+        <br />T T E R
+      </h2>
+      <div className="bg-white container p-12 rounded-3xl flex sm:flex-col md:flex-row gap-16 md:w-11/12 sm:w-full mb-20">
+        <div className="flex-1">
+          <div className="mb-12 mt-4 w-full">
+            <label
+              htmlFor="bill"
+              className="block text-sm font-bold mb-2 text-dark-grayish-cyan"
+            >
+              Bill
+            </label>
+            <div className="bg-very-light-grayish-cyan rounded-lg relative">
+              <Image
+                src={DollarIcon}
+                className="text-bold m-4 inline absolute left-0 -top-0 h-4 w-4"
+                height={12}
+                width={12}
+                alt="dolar sign icon"
+              />
+              <input
+                id="bill"
+                name="bill"
+                type="number"
+                value={totalAmount}
+                onChange={handleTotalAmount}
+                className="row-span-2 text-right bg-very-light-grayish-cyan w-full rounded-md h-12 pr-8 text-very-dark-cyan focus:ring-2 focus:ring-primary focus:outline-none focus:border-transparent"
+              />
+            </div>
+            <div className="my-12">
+              <label
+                htmlFor="tip"
+                className="block text-sm font-bold mb-2 text-dark-grayish-cyan"
+              >
+                Select Tip %
+              </label>
+              <div className="grid grid-cols-3 gap-4">
+                <button className="bg-very-dark-cyan text-2xl p-3 rounded-lg text-center text-white hover:bg-light-grayish-cyan hover:text-very-dark-cyan active:bg-primary active:text-very-dark-cyan">
+                  5%
+                </button>
+                <button className="bg-very-dark-cyan text-2xl p-3 rounded-lg text-center text-white hover:bg-light-grayish-cyan hover:text-very-dark-cyan active:bg-primary active:text-very-dark-cyan">
+                  10%
+                </button>
+                <button className="bg-very-dark-cyan text-2xl p-3 rounded-lg text-center text-white hover:bg-light-grayish-cyan hover:text-very-dark-cyan active:bg-primary active:text-very-dark-cyan">
+                  15%
+                </button>
+                <button className="bg-very-dark-cyan text-2xl p-3 rounded-lg text-center text-white hover:bg-light-grayish-cyan hover:text-very-dark-cyan active:bg-primary active:text-very-dark-cyan">
+                  25%
+                </button>
+                <button className="bg-very-dark-cyan text-2xl p-3 rounded-lg text-center text-white hover:bg-light-grayish-cyan hover:text-very-dark-cyan active:bg-primary active:text-very-dark-cyan">
+                  50%
+                </button>
+                <input
+                  className="bg-very-light-grayish-cyan text-2xl p-2 rounded-lg text-center text-dark-grayish-cyan placeholder:text-dark-grayish-cyan focus:ring-2 focus:ring-primary focus:outline-none focus:border-transparent"
+                  type="number"
+                  placeholder="Custom"
+                  id="tip"
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="number-people"
+                className="block text-sm font-bold mb-2 text-dark-grayish-cyan"
+              >
+                Number of People
+              </label>
+              <div className="bg-very-light-grayish-cyan rounded-lg relative">
+                <Image
+                  src={PeopleIcon}
+                  className="text-bold m-4 inline absolute left-0 -top-0 h-4 w-4"
+                  height={12}
+                  width={12}
+                  alt="people sign icon"
+                />
+                <input
+                  id="bill"
+                  name="bill"
+                  type="number"
+                  className="row-span-2 text-right bg-very-light-grayish-cyan w-full rounded-md h-12 pr-8 text-very-dark-cyan focus:ring-2 focus:ring-primary focus:outline-none focus:border-transparent"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-very-dark-cyan rounded-2xl text-white px-12 py-8 relative flex-1">
+          <div className="grid grid-cols-2 grid-rows-3">
+            <div className="row-span-1">
+              <p className="text-md">Tip Amount</p>
+              <p className="text-sm text-dark-grayish-cyan">/ person</p>
+            </div>
+            <p className="text-4xl row-span-2 text-right text-primary">$4.27</p>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-3">
+            <div>
+              <p className="text-md">Total</p>
+              <p className="text-sm text-dark-grayish-cyan">/ person</p>
+            </div>
+            <h1 className="text-4xl row-span-2 text-right text-primary">
+              $32.79
+            </h1>
+          </div>
+          <button className="w-full bg-primary py-3 rounded-md text-very-dark-cyan hover:bg-light-grayish-cyan">
+            RESET
+          </button>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="attribution">
+        Challenge by{" "}
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+          Frontend Mentor
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        . Coded by <a href="https://alananaya.dev">Alan Anaya</a>.
       </div>
     </main>
   );
